8f99ebd2cf644aa228c11c4d398b8a0fe8b41119
Erl Jerrald Jocson <Erl.Jocson@teamhgs.com>
2025-12-08 13:03:35 +0800

    modified project name

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+import os
 import app
-from pathlib import Path
 from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == '__main__':
     print(datetime.now().strftime("%B %d, %Y %I:%M %p"))
 
-    directory = Path(r"C:\Users\erl.jocson\Downloads")
-    tracker_directory = Path(r"R:\BE and Transformation Services\Analytics\04 Team Folder\Jocson, Erl Jerrald\Training Dashboard Data Tracker\Data Loaded Tracker.xlsx")
-
     app.run(
-        directory = directory,
-        tracker_directory = tracker_directory
+        directory = os.getenv("MAIN_DIRECTORY"),
+        tracker_directory = os.getenv("TRACKER_FILE_DIRECTORY")
     )
     
